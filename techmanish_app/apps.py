035a42f09093d46eedf5e964e55e71604fe3c61c
hellotechmanish <mk772542@gmail.com>
2023-06-13 20:46:39 +0530

    upload all files

---
+++ techmanish_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TechmanishAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'techmanish_app'
---
+++ techmanish_app/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+def profile(request):
+    return render(request,'profile.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def help(request):
+    return render(request,'help.html')
